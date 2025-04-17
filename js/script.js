bdb76e79e47013f77ab5f6b1211a5ff4471c3270
Catherine Vu <catherine.vu@stu.ocsb.ca>
2025-04-17 16:52:25 +0000

    added html

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Catherine Vu All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Catherine
+// Created on: April 17 2025
 // This file contains the JS functions for index.html
+
+const randomNumber = Math.floor(Math.random() * 6) + 1;
+
+/**
+ * This function checks if the user has selected the random number from above
+ */
+function checkNumber () {
+  const userNumber = parseInt(document.getElementById('user-number').value);
+
+  if (userNumber === randomNumber) {
+    document.getElementById('result').innerHTML =
+      '<p>You have guessed the correct number!</p>';
+  } else {
+    document.getElementById('result').innerHTML =
+      '<p>You have guessed the wrong number!' +
+      '<br/>The correct number was: ' + randomNumber + '</p>';
+  }
+}
